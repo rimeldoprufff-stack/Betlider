a96b7c2fc1ea0a5f36aefce4336f8c930c0a0f80
rimeldoprufff-stack <rimeldoprufff@gmail.com>
2026-02-03 21:14:31 +0200

    Delete config.js

--- config.js
+++
@@ -1,5 +0,0 @@
-/* BTLIDER config (root) */
-window.BTLIDER_CONFIG = window.BTLIDER_CONFIG || {};
-window.BTLIDER_CONFIG.supabaseUrl = "https://vshzqiqppgitnhuqmcgn.supabase.co";
-window.BTLIDER_CONFIG.supabaseAnonKey = "sb_publishable_DJSaoIKQQPU_5rio3xt8ZQ_8H_6Xs4X";
-/* NOTE: anon/publishable key is OK for client-side use. Do NOT put service_role key on GitHub. */
